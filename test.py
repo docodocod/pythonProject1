f549e0091646762d4e565645fb73f619ba2a0223
docodocod <97988384+docodocod@users.noreply.github.com>
2022-07-21 10:24:02 +0900

    Update test.py

--- test.py
+++ test.py
@@ -1,6 +1,10 @@
 import requests
 
 url="http://ddragon.leagueoflegends.com/cdn/12.13.1/data/en_US/champion/Aatrox.json"
+url2="http://ddragon.leagueoflegends.com/cdn/12.13.1/data/en_US/champion/Ahri.json"
+
 res=requests.get(url).json()
+res2=requests.get(url2).json()
 
 champ_lst=(res['data']['Aatrox']['key'],res['data']['Aatrox']['name'])
+champ_lst2=(res2['data']['Aatrox']['key'],res['data']['Aatrox']['name'])
